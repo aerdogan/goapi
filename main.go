9a1b2a751a73321169523286e1d8e5184985f214
Ahmet Erdoğan <tuxposta@gmail.com>
2022-12-16 02:01:20 +0300

    Fiber Very Cool

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"goapi/routes"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cache"
+	"github.com/gofiber/fiber/v2/middleware/compress"
+	"github.com/gofiber/fiber/v2/middleware/limiter"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+)
+
+func main() {
+	app := fiber.New()
+
+	// app.Use(cors.New())     // cors
+	// app.Use(csrf.New())     // csrf
+	app.Use(limiter.New())  // limiter
+	app.Use(logger.New())   // logger
+	app.Use(cache.New())    // cache
+	app.Use(compress.New()) // compression
+
+	routes.UserRoute(app)
+	app.Listen(":3000")
+}
