1b442cd1f230f4e81d5d35b6112adfbfb0b55f25
Ahmet Erdoğan <tuxposta@gmail.com>
2022-12-16 02:38:02 +0300

    Refactor

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cache"
 	"github.com/gofiber/fiber/v2/middleware/compress"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/csrf"
 	"github.com/gofiber/fiber/v2/middleware/limiter"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 )
@@ -13,8 +15,8 @@ import (
 func main() {
 	app := fiber.New()
 
-	// app.Use(cors.New())     // cors
-	// app.Use(csrf.New())     // csrf
+	app.Use(cors.New())     // cors
+	app.Use(csrf.New())     // csrf
 	app.Use(limiter.New())  // limiter
 	app.Use(logger.New())   // logger
 	app.Use(cache.New())    // cache
